cd5b31f33d0bd7f30a86ff605849fb6f7693dfef
rupa <rupa@lrrr.us>
2009-06-18 01:05:40 -0400

    initial commit - the new j, still z while I fiddle some more

---
+++ z.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+'''
+maintains a jump-list of the directories you actually use
+old directories eventually fall off the list
+inspired by Joel Schaerer's http://wiki.github.com/joelthelion/autojump
+and something similar i had - but i could never get the dir list right.
+
+INSTALL:
+  source j.sh into .bashrc under your '[-z "$PS1" ] || return' line
+  cd around for a while
+
+TIPS:
+  if you opten prefer a non-default match type, use aliases:
+    alias jl='j -l'
+    alias jt='j -t recent'
+    alias jr='j -r rank'
+
+CREDITS:
+  Joel Schaerer aka joelthelion for autojump
+  Daniel Drucker aka dmd for finding bugs and making me late for lunch
+'''
+
+import os, sys, time
+
+class J(object):
+
+    def __init__(self, datafile):
+        ''' datafile format: path|rank|atime '''
+        self.datafile = datafile
+        self.common = None
+        self.args = []
+        self.m = []
+        self.ordered = { 'rank' : self.rank,
+                         'recent' : self.recent,
+                         'frecent': self.frecent }
+        # get list
+        with open(self.datafile, 'r') as f:
+            self.d = [l.strip().split('|') for l in f.readlines()]
+        self.d = [d for d in self.d if os.path.exists(d[0])]
+
+        # rewrite to disk
+        with open(self.datafile, 'w') as f:
+            f.write('\n'.join(['|'.join(d) for d in self.d]))
+            f.write('\n')
+
+    def pretty(self, order):
+        ''' return a listing by order '''
+        if order not in self.ordered:
+            return ''
+        r = ['by %s' % order]
+        r.extend(['%-15s %s' % i for i in self.ordered[order]()])
+        return '\n'.join(r)
+
+    def go(self, order):
+        ''' go by order '''
+        if order == 'common':
+            return self.common
+        if self.m and order in self.ordered:
+            return self.ordered[order]()[-1][1]
+
+    def rank(self):
+        ''' time spent/aging, taken care of in .sh '''
+        r = ([(i[0], i[2]) for i in self.m])
+        return sorted(r)
+
+    def recent(self):
+        ''' by recently accessed '''
+        r = ([(i[1], i[2]) for i in self.m])
+        return sorted(r, reverse=True)
+
+    def frecent(self):
+        ''' rank weighted by recently accessed '''
+        r = []
+        for i in self.m:
+            if i[1] <= 3600:
+                r.append((i[0]*10, i[2]))
+            elif i[1] <= 86400:
+                r.append((i[0]*2, i[2]))
+            elif i[1] <= 604800:
+                r.append((i[0]/2, i[2]))
+            else:
+                r.append((i[0]/10, i[2]))
+        return sorted(r)
+
+    def matches(self, args, nocase=False):
+        '''
+        set self.m to a list of possibly case sensitive path matches
+        m format: (rank, atime, path)
+        '''
+
+        def common(r, l, nocase):
+            '''
+            return prefix if there's a common prefix to all matches,
+            the prefix is in the list,
+            and all the args match in it
+            '''
+            pref = os.path.commonprefix([i[2] for i in r])
+            if not pref or pref == '/':
+                return None
+            r = [i for i in r if i[2] == pref]
+            if not r:
+                return None
+            if nocase:
+                pref = pref.lower()
+            for i in l:
+                if i not in pref:
+                    return None
+            return r[0][2]
+
+        def cmpare(str, l, nocase):
+            ''' every item in list l must match in string str '''
+            match = True
+            if nocase:
+                str = str.lower()
+            for i in l:
+                if i not in str:
+                    match = False
+            return match
+
+        self.args, self.m = args, []
+        if nocase:
+            args = [i.lower() for i in args]
+        for d in self.d:
+            if not cmpare(d[0], args, nocase):
+                continue
+            self.m.append((float(d[1]),
+                           int(time.time())-int(d[2]),
+                           d[0]))
+        self.common = common(self.m, args, nocase)
+        return self.m
+
+def main(file, list, type, args):
+    '''
+    make sure the only thing that gets to stdout is a place to cd
+    '''
+    if not file:
+        return
+
+    # if we hit enter on a completion, go there
+    if args and os.path.isdir(args[-1]):
+        sys.stdout.write(os.path.realpath(args[-1]))
+        return
+
+    j = J(file)
+
+    # prefer case sensitive
+    if not j.matches(args):
+        j.matches(args, True)
+
+    if list or not args:
+        sys.stderr.write(j.pretty(type) + '\n')
+        if j.common:
+            sys.stderr.write('common: %s\n' % j.common)
+    # if all our args match a common prefix, let's go there
+    elif j.common:
+        sys.stderr.write(j.pretty(type) + '\n')
+        sys.stderr.write('common: %s\n' % j.common)
+        sys.stdout.write(j.common)
+    else:
+        go = j.go(type)
+        if not go:
+            return
+        sys.stderr.write(j.pretty(type) + '\n')
+        sys.stdout.write(go)
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser(usage='%prog -f datafile [options] args')
+    parser.add_option('-f', '--file', help='data file')
+    parser.add_option('-l', action='store_true', default=False,
+                      help='list matches')
+    parser.add_option('-t', default='frecent', help='match type [frecent (default), rank, recent]')
+    parser.add_option('-V', action='store_true', default=False,
+                      help='documentation')
+    saveout = sys.stdout
+    sys.stdout = sys.stderr
+    opts, args = parser.parse_args()
+    if opts.V:
+        sys.stdout.write(__doc__)
+    elif not opts.file:
+        parser.error('data file required (-f file)')
+    elif opts.t not in ['frecent', 'rank', 'recent']:
+        parser.print_help()
+    else:
+        sys.stdout = saveout
+        main(opts.file, opts.l, opts.t, args)
---
+++ z.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+z() {
+ local datafile=$HOME/.z
+ if [ "$1" = "--add" ]; then
+  shift
+  # $HOME isn't worth matching
+  [ "$*" = "$HOME" ] && return
+  awk -v q="$*" -v t="$(date +%s)" -F"|" '
+   BEGIN { l[q] = 1; d[q] = t }
+   $2 >= 1 {
+    if( $1 == q ) {
+     l[$1] = $2 + 1
+     d[$1] = t
+    } else {
+     l[$1] = $2
+     d[$1] = $3
+    }
+    count += $2
+   }
+   END {
+    if( count > 1000 ) {
+     for( i in l ) print i "|" 0.9*l[i] "|" d[i] # aging
+    } else for( i in l ) print i "|" l[i] "|" d[i]
+   }
+  ' $datafile 2>/dev/null > $datafile.tmp
+  mv -f $datafile.tmp $datafile
+ elif [ "$1" = "--complete" ]; then
+  # tab completion
+  awk -v q="$2" -F"|" '
+   BEGIN { split(substr(q,3),a," ") }
+   {
+    if( system("test -d \"" $1 "\"") ) next
+    for( i in a ) $1 !~ a[i] && $1 = ""; if( $1 ) print $1
+   }
+  ' $datafile 2>/dev/null
+ else
+  # list/go
+  local pyfile="$HOME/Documents/dev/jj/z.py"
+  cd="$($pyfile -f $datafile $*)"
+  [ -z "$cd" -o "$cd" = "$PWD" ] && return
+  cd $cd
+ fi
+}
+# tab completion
+complete -C 'z --complete "$COMP_LINE"' z
+# populate directory list. avoid clobbering other PROMPT_COMMANDs.
+PROMPT_COMMAND='z --add "$(pwd -P)";'"$PROMPT_COMMAND"
